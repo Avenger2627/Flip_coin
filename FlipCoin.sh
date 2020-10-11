ab14c67eb4364da995eb4e3d739650707e133865
Avenger2627 <imraninfo97@gmail.com>
2020-10-11 15:33:47 +0530

    Added:case 1_Stroring_Resultin_Deictionay

--- FlipCoin.sh
+++ FlipCoin.sh
@@ -1,3 +1,15 @@
+  
 #!/bin/bash -x
+echo "------------------------------- Well come flip coin simulator ---------------------------"
 
-echo "_________________Welcome to Flic Coin_________"
+#CONSTANT
+HEAD=0
+
+#TO PRINT THE HEAD AND TAIL
+flipCoin=$((RANDOM%2))
+if [ $flipCoin == $HEAD ]
+then
+	echo "Head"
+else
+	echo "Tail"
+fi
